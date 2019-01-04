a48d8c3cb66efae481bd7a90fbc532559e86aef0
Aleksandrs Gusevs <aleksandrs.gusevs@proofit.lv>
2019-01-05 00:11:26 +0200

    Fixed main test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,19 +1,24 @@
+import static org.mockito.ArgumentMatchers.any;
+import static org.powermock.api.mockito.PowerMockito.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import org.telegram.telegrambots.meta.generics.BotSession;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 import shrimpbot.ShrimpBot;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ Guice.class, TelegramBotsApi.class})
+@PrepareForTest({ Main.class, Guice.class })
 public class MainTest {
 
 	@Mock
@@ -23,15 +28,30 @@ public class MainTest {
 	private Injector injector;
 
 	@Mock
+	private TelegramBotsApi telegramBotsApi;
+
+	@Mock
 	private BotSession botSession;
 
 	@Test
-	public void main() throws TelegramApiRequestException {
-//		TelegramBotsApi telegramBotsApi = PowerMockito.mock(TelegramBotsApi.class);
-//		PowerMockito.mockStatic(Guice.class);
-//		when(Guice.createInjector(any(AbstractModule.class))).thenReturn(injector);
-//		when(injector.getInstance(ShrimpBot.class)).thenReturn(shrimpBot);
-//		when(telegramBotsApi.registerBot(any(LongPollingBot.class))).thenReturn(botSession);
+	public void main() throws Exception {
+		PowerMockito.mockStatic(Guice.class);
+		whenNew(TelegramBotsApi.class).withAnyArguments().thenReturn(telegramBotsApi);
+		when(telegramBotsApi.registerBot(any(ShrimpBot.class))).thenReturn(botSession);
+		when(Guice.createInjector(any(AbstractModule.class))).thenReturn(injector);
+		when(injector.getInstance(ShrimpBot.class)).thenReturn(shrimpBot);
+
+		String[] args = {};
+		Main.main(args);
+	}
+
+	@Test
+	public void mainThrowsException() throws Exception {
+		PowerMockito.mockStatic(Guice.class);
+		whenNew(TelegramBotsApi.class).withAnyArguments().thenReturn(telegramBotsApi);
+		doThrow(new TelegramApiRequestException("Request exception")).when(telegramBotsApi).registerBot(shrimpBot);
+		when(Guice.createInjector(any(AbstractModule.class))).thenReturn(injector);
+		when(injector.getInstance(ShrimpBot.class)).thenReturn(shrimpBot);
 
 		String[] args = {};
 		Main.main(args);
